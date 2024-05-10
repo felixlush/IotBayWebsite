65c13fb65126a40d8cbb48211960a0cfc5b64904
felixlush <92777513+felixlush@users.noreply.github.com>
2024-05-10 14:38:27 +1000

    Fixed indenting LoginServlet

--- src/java/iot/isd/controller/LoginServlet.java
+++ src/java/iot/isd/controller/LoginServlet.java
@@ -2,28 +2,28 @@ package iot.isd.controller;
 
  
 
-  import java.io.IOException;
+import java.io.IOException;
 
-    import java.sql.SQLException;
+import java.sql.SQLException;
 
-    import java.util.logging.Level;
+import java.util.logging.Level;
 
-    import java.util.logging.Logger;
+import java.util.logging.Logger;
 
-    import javax.servlet.ServletException;
+import javax.servlet.ServletException;
 
-    import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
 
-    import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
-    import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
-    import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 
-    import iot.isd.model.User;
+import iot.isd.model.User;
 
-    import iot.isd.model.dao.DBManager;
-    import iot.isd.model.User;
+import iot.isd.model.dao.DBManager;
+import iot.isd.model.User;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
